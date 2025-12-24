8c274b7558a0a101d6bf511fe323c8ffa0273592
Arianna Masciolini <uzkamascio@gmail.com>
2025-12-24 19:07:25 +0100

    remove redundant stuff

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import Recipe, { loaderData } from "./routes/recipe";
+import Recipe from "./routes/recipe";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello World.</div>,
   },
-  { path: "/recipes/:recipe", element: <Recipe />, loader: loaderData },
+  { path: "/recipes/:recipe", element: <Recipe /> },
 ]);
 const root = document.getElementById("root")!;
 
--- src/routes/recipe.tsx
+++ src/routes/recipe.tsx
@@ -1,19 +1,7 @@
 import { useEffect, useState } from "react";
-import { useLoaderData } from "react-router";
-
-export async function loader({ params }) {
-  // console.log(`"What are you ${params}`);
-  const markdownFileContent = (
-    await import(`../public/recipes/red-beet-soup.md?raw`)
-  ).default;
-  console.log(markdownFileContent);
-  console.log("------------loaderdata----");
-  return { markdownFileContent };
-}
 
 export default function Recipe() {
-  const { recipe } = useLoaderData();
-  const [data, setData] = useState("");
+  const [recipe, setData] = useState("");
 
   useEffect(() => {
     const markdownFileContent = async () => {
@@ -22,12 +10,10 @@ export default function Recipe() {
       setData(res);
     };
     markdownFileContent();
-  }, [data]);
+  }, [recipe]);
   return (
     <>
-      <p> are you empty</p>
       <p>{recipe}</p>
-      <p>{data}</p>
     </>
   );
 }
