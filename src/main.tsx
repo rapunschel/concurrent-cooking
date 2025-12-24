24e2ab97e5e699f229c5077507cbf8b8f52e3343
rapunschel <rapunschel.gaming@gmail.com>
2025-12-24 19:24:45 +0100

    rename: recipeData -> recipeLoader

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import Recipe, { loader as recipeData } from "./routes/recipe";
+import Recipe, { loader as recipeLoader } from "./routes/recipe";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello World.</div>,
   },
-  { path: "/recipes/:recipeId", element: <Recipe />, loader: recipeData },
+  { path: "/recipes/:recipeId", element: <Recipe />, loader: recipeLoader },
 ]);
 const root = document.getElementById("root")!;
 
