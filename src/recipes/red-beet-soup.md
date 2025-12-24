d517a1f189f4e7b805d5f04e06a04b6037bf9e1a
Arianna Masciolini <uzkamascio@gmail.com>
2025-12-24 21:06:44 +0100

    WIP render recipes but regret steps format

--- src/recipes/red-beet-soup.md
+++ src/recipes/red-beet-soup.md
@@ -22,15 +22,16 @@ threads:
 - hand blender
 
 ## Steps
-1. multithreading:
-  - 1. chop the onion finely
+1. idk
+  - idk
+    1. chop the onion finely
     2. sauté the onion in some oil inside a tall pot
   - dice the red beets and the potatoes
-2. multithreading:
+2. idk   
    - add the vegetables to the pot and stir for about 5 minutes
    - warm the broth up
 3. pour the broth into the pot and bring it to a boil. Let everything cook for at least 15-20 minutes, until both the beets and the potatoes are soft
-4. multithreading:
+4. idk
    - grate a generous amount of cheese
    - turn off the stove and blend everything together
 5. season with salt, pepper and optionally some extra oil and a bit of yogurt or sour cream
--- src/routes/recipe.tsx
+++ src/routes/recipe.tsx
@@ -1,17 +1,51 @@
 import { useLoaderData } from "react-router";
+import parse from 'html-react-parser';
+import { getMarkdownMetadata } from "ts-markdown-parser";
+import { elementToHtml, parseMarkdown } from "ts-markdown-parser/utils/markdown-parser.js";
 
 export async function loader({ params }: any) {
-  const recipe = (await import(`../recipes/${params.recipeId}.md?raw`))
+  const md = (await import(`../recipes/${params.recipeId}.md?raw`))
     .default;
-  return { recipe };
+  return { md };
 }
 
-export default function Recipe() {
-  const { recipe } = useLoaderData();
+export default function md() {
+  const { md } = useLoaderData();
+  const meta = getMarkdownMetadata(md);
+  const title = meta.title;
+  let contents = "";
+  const tags = meta.tags
+    .replace(" ", "")
+    .split(",")
+    .map((tag: string): string => "#" + tag)
+    .join(" ");
+  const parsed = parseMarkdown(md);
+  console.log(parsed); // TODO: rm
+
+  let currSection = undefined;
+  for (const el in parsed) {
+    const content = parsed[el].content;
+    const type = parsed[el].type;
+    if (type == "h2") {
+      currSection = content;
+      contents += elementToHtml(parsed[el], {});
+    } else {
+      if (currSection == "Steps" && type == "ul") {
+        continue
+      } else if (content.includes("idk")) {
+        continue; // TODO: agree on how to render 
+      } else {
+        contents += elementToHtml(parsed[el], {});
+      }
+    }
+  }
+
 
   return (
     <>
-      <p>{recipe}</p>
+      <h1>{title}</h1>
+      <h4>{tags}</h4>
+      {parse(contents)}
     </>
   );
 }
