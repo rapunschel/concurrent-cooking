a00d9512e1c06b3b55d20ddc320bd5cb39b6ad23
Arianna Masciolini <uzkamascio@gmail.com>
2025-12-25 16:41:46 +0100

    better html for recipe page

--- src/routes/recipe.tsx
+++ src/routes/recipe.tsx
@@ -1,44 +1,88 @@
 import { useLoaderData } from "react-router";
 import parse from 'html-react-parser';
-import { getMarkdownMetadata, markdownToHtml } from "ts-markdown-parser";
+import { getMarkdownMetadata } from "ts-markdown-parser";
 import { elementToHtml, parseMarkdown } from "ts-markdown-parser/utils/markdown-parser.js";
+import type { Component, ReactElement } from "react";
 
 export async function loader({ params }: any) {
-  const md = (await import(`../recipes/${params.recipeId}.md?raw`))
+  const txt = (await import(`../recipes/${params.recipeId}.md?raw`))
     .default;
-  return { md };
+  return { txt };
 }
 
-export default function Recipe() {
-  const { md } = useLoaderData();
-  const meta = getMarkdownMetadata(md);
-  const title = meta.title;
-  const tags = meta.tags
+function Meta(fm: Record<string, any>): ReactElement {
+  const title = fm.title;
+  const user = fm.user;
+  const tags = fm.tags
     .replace(" ", "")
     .split(",")
     .map((tag: string): string => "#" + tag)
     .join(" ");
-
-  const parsed = parseMarkdown(md);
-  let currSection = undefined;
-  let contents = "";
-  for (const el in parsed) {
-    const content = parsed[el].content;
-    const type = parsed[el].type;
-    const html = elementToHtml(parsed[el], {});
-    if (type == "h2") {
-      currSection = content;
-    } else if (currSection == "Steps" && type == "ul") {
-      continue; // TODO: add class parallel to html element
-    }
-    contents += html;
-  }
-
+  const threads = fm.threads;
+  const tot_time = fm.time;
+  const hours = tot_time / 60;
+  const mins = tot_time % 60;
+  const time = (hours > 0 ? `${hours} hours` : "") + (mins > 0 ? `${mins} minutes` : "");
   return (
     <>
       <h1>{title}</h1>
-      <h4>{tags}</h4>
-      {parse(contents)}
+      <div id="meta">
+        <ul>
+          <li>tags: {tags}</li>
+          <li>user: {user}</li>
+          <li>threads: {threads}</li>
+          <li>time: {time}</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+function Step(lis: string, parallel: boolean = false, threads: number = 1): ReactElement {
+  return (
+    <>
+      <ul
+        className={(parallel ? "parallel" : "")}
+        style={{ display: "grid", gridTemplateColumns: `repeat(${threads},1fr)` }}>
+        {parse(lis)}
+      </ul >
+    </>
+  );
+}
+
+function Tag(content: string, type: string): ReactElement {
+  return <>{parse(`<${type}>${content}</${type}>`)}</>
+}
+
+function Body(md: Record<string, any>): ReactElement {
+  let currSection = undefined;
+  let contents: ReactElement[] = [];
+  for (const el in md) {
+    const content = md[el].content;
+    const type = md[el].type;
+    if (type == "h2") {
+      currSection = content;
+    }
+
+    if (currSection == "Steps" && type == "ul") {
+      contents.push(Step(content, true));
+    } else {
+      contents.push(Tag(content, type));
+    }
+  }
+  return <>{contents}</>;
+}
+
+export default function Recipe(): ReactElement {
+  const { txt } = useLoaderData();
+
+  const meta = Meta(getMarkdownMetadata(txt));
+  const body = Body(parseMarkdown(txt));
+
+  return (
+    <>
+      {meta}
+      {body}
     </>
   );
 }
