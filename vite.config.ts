53ae0c36e973e96ca02998e13de3bef451e20bb0
rapunschel <rapunschel.gaming@gmail.com>
2025-12-25 17:19:07 +0100

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/REPO_NAME/",
+  base: "/concurrent-cooking/",
   plugins: [
     react({
       babel: {
