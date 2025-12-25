abd8c7a09231a0dc3699435bcddca0e20ee9fcd7
Arianna Masciolini <uzkamascio@gmail.com>
2025-12-25 02:53:04 +0100

    much simpler format?

--- src/recipes/red-beet-soup.md
+++ src/recipes/red-beet-soup.md
@@ -22,16 +22,14 @@ threads:
 - hand blender
 
 ## Steps
-1. idk
-  - idk
-    1. chop the onion finely
-    2. sauté the onion in some oil inside a tall pot
-  - dice the red beets and the potatoes
-2. idk   
-   - add the vegetables to the pot and stir for about 5 minutes
-   - warm the broth up
-3. pour the broth into the pot and bring it to a boil. Let everything cook for at least 15-20 minutes, until both the beets and the potatoes are soft
-4. idk
-   - grate a generous amount of cheese
-   - turn off the stove and blend everything together
-5. season with salt, pepper and optionally some extra oil and a bit of yogurt or sour cream
+- chop the onion finely and sauté it in some oil inside a tall pot
+- dice the red beets and the potatoes
+
+- add the vegetables to the pot and stir for about 5 minutes
+- warm the broth up
+
+- pour the broth into the pot and bring it to a boil. Let everything cook for at least 15-20 minutes, until both the beets and the potatoes are soft
+- grate a generous amount of cheese
+
+- turn off the stove and blend everything together
+- season with salt, pepper and optionally some extra oil and a bit of yogurt or sour cream
--- src/routes/recipe.tsx
+++ src/routes/recipe.tsx
@@ -1,6 +1,6 @@
 import { useLoaderData } from "react-router";
 import parse from 'html-react-parser';
-import { getMarkdownMetadata } from "ts-markdown-parser";
+import { getMarkdownMetadata, markdownToHtml } from "ts-markdown-parser";
 import { elementToHtml, parseMarkdown } from "ts-markdown-parser/utils/markdown-parser.js";
 
 export async function loader({ params }: any) {
@@ -9,37 +9,30 @@ export async function loader({ params }: any) {
   return { md };
 }
 
-export default function md() {
+export default function Recipe() {
   const { md } = useLoaderData();
   const meta = getMarkdownMetadata(md);
   const title = meta.title;
-  let contents = "";
   const tags = meta.tags
     .replace(" ", "")
     .split(",")
     .map((tag: string): string => "#" + tag)
     .join(" ");
-  const parsed = parseMarkdown(md);
-  console.log(parsed); // TODO: rm
 
+  const parsed = parseMarkdown(md);
   let currSection = undefined;
+  let contents = "";
   for (const el in parsed) {
     const content = parsed[el].content;
     const type = parsed[el].type;
+    const html = elementToHtml(parsed[el], {});
     if (type == "h2") {
       currSection = content;
-      contents += elementToHtml(parsed[el], {});
-    } else {
-      if (currSection == "Steps" && type == "ul") {
-        continue
-      } else if (content.includes("idk")) {
-        continue; // TODO: agree on how to render 
-      } else {
-        contents += elementToHtml(parsed[el], {});
-      }
+    } else if (currSection == "Steps" && type == "ul") {
+      continue; // TODO: add class parallel to html element
     }
+    contents += html;
   }
-
 
   return (
     <>
