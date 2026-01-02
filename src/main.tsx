b244d9cb5c259c2d0cc4bda6f79bbfa621882406
rapunschel <93683352+rapunschel@users.noreply.github.com>
2026-01-02 20:00:53 +0100

    add routing for tags

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Recipe, { loader as recipeLoader } from "./routes/recipe";
-import Home from "./routes/home";
+import Home, { loader as homeLoader } from "./routes/home";
 import "./style.css";
 
 const router = createBrowserRouter([
@@ -11,6 +11,12 @@ const router = createBrowserRouter([
     path: "/concurrent-cooking",
     element: <Home />,
   },
+  {
+    path: "/concurrent-cooking/:tagId",
+    element: <Home />,
+    loader: homeLoader,
+  },
+
   {
     path: "/concurrent-cooking/recipes/:recipeId",
     element: <Recipe />,
