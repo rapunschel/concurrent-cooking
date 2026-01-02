a25e0d6e2c766ae1cd7201301715229c70e3271b
rapunschel <93683352+rapunschel@users.noreply.github.com>
2026-01-03 00:03:47 +0100

    Added loader for home to get all recipes, navigation from '/all' renavigates to home

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Recipe, { loader as recipeLoader } from "./routes/recipe";
-import Home from "./routes/home";
+import Home, { loader as homeLoader } from "./routes/home";
 import Terminal, { loader as terminalLoader } from "./components/Terminal.jsx";
 
 import "./style.css";
@@ -16,7 +16,11 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Terminal />,
-        loader: terminalLoader,
+        loader: homeLoader,
+      },
+      {
+        path: "all",
+        element: <Navigate to="/concurrent-cooking" replace />,
       },
       {
         path: "/concurrent-cooking/:tagId",
