25a7b1cb5c536e0db32180f9319422282bceaa51
rapunschel <93683352+rapunschel@users.noreply.github.com>
2026-01-02 20:13:31 +0100

    Fix routing + refactor out terminal into its own file

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,27 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Recipe, { loader as recipeLoader } from "./routes/recipe";
-import Home, { loader as homeLoader } from "./routes/home";
+import Home from "./routes/home";
+import Terminal, { loader as terminalLoader } from "./components/Terminal.jsx";
+
 import "./style.css";
 
 const router = createBrowserRouter([
   {
     path: "/concurrent-cooking",
     element: <Home />,
-  },
-  {
-    path: "/concurrent-cooking/:tagId",
-    element: <Home />,
-    loader: homeLoader,
+    children: [
+      {
+        index: true,
+        element: <Terminal />,
+        loader: terminalLoader,
+      },
+      {
+        path: "/concurrent-cooking/:tagId",
+        element: <Terminal />,
+        loader: terminalLoader,
+      },
+    ],
   },
 
   {
